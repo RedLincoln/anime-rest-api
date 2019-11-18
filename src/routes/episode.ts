3206cf8c53ac3bf74e342a4cd95fba565ff20689
RedLincoln <miguel_lcp@hotmail.com>
2019-11-18 21:11:10 +0000

    delete and updated added to episode route

--- src/routes/episode.ts
+++ src/routes/episode.ts
@@ -1,5 +1,5 @@
 import express, { Router, Response, Request } from 'express'
-import { Episode, EpisodeProp } from '../models/episode'
+import { Episode, EpisodeProp, EpisodeDoc } from '../models/episode'
 import { CustomRequest, getAnime } from './partials'
 
 const router: Router = express.Router()
@@ -35,5 +35,42 @@ router.get('/:name/:number', getAnime, async (req: CustomRequest, res: Response)
     }
 })
 
+router.post('/:name', getAnime, async (req: Request, res: Response) => {
+    try {
+        const episode: EpisodeDoc = new Episode(req.body)
+        const newEpisode: EpisodeProp = await episode.save()
+        res.json(newEpisode)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+router.patch('/:name/:number', getAnime, async (req: CustomRequest, res: Response) => {
+    try {
+        const episode = await Episode.findOneAndUpdate({
+            anime: req.anime?._id,
+            number: parseFloat(req.params.number)
+        }, req.body, { new: true })
+        if (episode) {
+            res.json(episode)
+        } else {
+            res.status(404).json({ message: `Episode ${req.params.number} of Anime ${req.params.name} not found` })
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+router.delete('/:name/:number', getAnime, async (req: CustomRequest, res: Response) => {
+    try {
+        await Episode.deleteOne({
+            anime: req.anime?._id,
+            number: parseFloat(req.params.number)
+        })
+        res.json({ message: `Episode ${req.params.number} of Anime ${req.params.name} deleted` })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 export { router as EpisodeRouter }
